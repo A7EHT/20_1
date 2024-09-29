22bc68ff8fb91ef8b6cb4b339c49732a9434d80c
Tiki <165913650+A7EHT@users.noreply.github.com>
2024-09-29 19:34:54 +0500

    fix dz

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -14,6 +14,6 @@ def contacts(request):
 
 
 def product_detail(request, pk):
-    product = Product.objects.filter(pk=pk).first()
+    product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
     return render(request, 'catalog/product_detail.html', context=context)
